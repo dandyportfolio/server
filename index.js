822072a56cd7b0e0243f3921485ae7d99cfc692f
Dandy <dandyjreyes@gmail.com>
2020-08-16 22:16:31 -0700

    removed handlebars

--- index.js
+++ index.js
@@ -14,31 +14,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/send', async ({ body: { name, email, bodyText } }, res) => {
-  const output = `
-  <p>Email from My Portfolio</p>
-  <h3>Contact Details</h3>
-  <ul>
-    <li>Name: ${name}</li>
-    <li>Email: ${email}</li>   
-  </ul>
-  <h3>Message</h3>
-  <p>${bodyText}</p>
-  `;
-
   try {
+    const output = `
+      <p>Email from My Portfolio</p>
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${name}</li>
+        <li>Email: ${email}</li>   
+      </ul>
+      <h3>Message</h3>
+      <p>${bodyText}</p>
+      `;
+
+    const { EMAIL, PASSWORD } = process.env;
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
+        user: EMAIL,
+        pass: PASSWORD,
       },
     });
 
     // send mail with defined transport object
     const info = await transporter.sendMail({
-      from: `"Personal Portfolio Contact" <${process.env.EMAIL}>`, // sender address
-      to: process.env.EMAIL, // list of receivers
+      from: `"Personal Portfolio Contact" <${EMAIL}>`, // sender address
+      to: EMAIL, // list of receivers
       subject: 'Message from Portfolio', // Subject line
       text: 'Hello world?', // plain text body
       html: output, // html body
@@ -56,6 +57,7 @@ app.post('/send', async ({ body: { name, email, bodyText } }, res) => {
     console.log(err);
   }
 });
+
 app.listen(process.env.PORT || 5000, () => {
   console.log('Running Server on port 5000');
 });
